d0056b80630a5eaa454861e49106bd700506aff8
Dorian CARDOSO <doriancardoso00@gmail.com>
2025-06-23 17:10:16 +0200

    removing init.sql and seed.sql to let ORM initializing the database.

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "⏳ Waiting for MySQL at tcp:mysql:3306..."
+npx wait-on tcp:mysql:3306
+
+echo "✅ MySQL is up!"
 
 echo "⏳ Applying Prisma schema to database..."
 npx prisma db push
 
+echo "🌱 Seeding database..."
+node prisma/seed.js || echo "❌ Seed failed"
+
 echo "🚀 Starting API server..."
-npm run start
+npm start
--- docker/mysql/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (lastname, firstname, email, password, role_id)
-VALUES
-    ('Doe', 'John', 'john.doe@example.com', '$2b$10$.tLd5jVIevXlDaHxPura0u1NNUB5OgLIw7QEDTJZlR9jy345pLTJe', 1),  -- admin
-    ('Smith', 'Jane', 'jane.smith@example.com', '$2b$10$.tLd5jVIevXlDaHxPura0u1NNUB5OgLIw7QEDTJZlR9jy345pLTJe', 2); -- user
